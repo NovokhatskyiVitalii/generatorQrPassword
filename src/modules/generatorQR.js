34d5d5d677a50612602b8dca321a3fe8ead62efa
Vitalii <tomrain78@gmail.com>
2023-07-20 20:00:58 +0300

    adding customDropDown

--- src/modules/generatorQR.js
+++ src/modules/generatorQR.js
@@ -1,5 +1,6 @@
 const customPicker = document.querySelectorAll(".custom-picker");
 const colorPicker = document.querySelectorAll(".color-picker");
+const customDropDown = document.querySelectorAll(".custom-dropdown");
 
 customPicker.forEach((item) => {
   item.addEventListener("click", () => {
@@ -14,5 +15,24 @@ colorPicker.forEach((item) => {
     let input = item.parentElement.querySelector("input[type=text]");
     span.style.backgroundColor = color;
     input.value = color;
+  });
+});
+
+customDropDown.forEach((item) => {
+  //select all options inside a custom dropdown
+  let options = item.querySelectorAll(".option");
+  //add event listener on all options
+  options.forEach((option) => {
+    option.addEventListener("click", () => {
+      //select all options of current options to remove active class
+      let allOptions = option.parentElement.querySelectorAll(".option");
+      allOptions.forEach((item) => {
+        item.classList.remove("active");
+      });
+      //add active on click
+      option.classList.add("active");
+      //update selected text
+      item.querySelector(".selected").innerHTML = option.innerHTML;
+    });
   });
 });
